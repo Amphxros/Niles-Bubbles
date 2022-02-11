34cb5649151010dd6410c8b593c4eb38b459e33d
Amphxros <amprubio@ucm.es>
2022-02-11 15:00:59 +0100

    Grid reader en un principio

--- Pharadrop/Assets/Scripts/GridParser/GridReader.cs
+++ Pharadrop/Assets/Scripts/GridParser/GridReader.cs
@@ -4,38 +4,57 @@ using UnityEngine;
 using System.IO;
 using System.Globalization;
 using System;
-public enum TypeLevel{ BOSS, MOVS, TEMP, SCORE};
+public enum TypeLevel{NONE, BOSS, MOVS, TEMP, SCORE};
 
 
-public class GridReader : MonoBehaviour
+public class GridReader
 {
     private static TextInfo Text = new CultureInfo("en-US", false).TextInfo;
     private  TypeLevel mLevelType_;   
     private int arg=-1;
+    int fils=0;
+    int cols=0;
     
-    public void readFile(string filename){
+    public GridReader(){
+        arg=-1;
+        mLevelType_= TypeLevel.NONE;
+
+    }
+    public TypeLevel getLevelType(){ return mLevelType_;}
+
+    public string[] readFile(string filename){
         StreamReader read= new StreamReader(filename);
         string type="";
         if(File.Exists(filename)){
-            int fils=0;
-            int cols=0;
 
             string [] tmp= new string[100];
             type= read.ReadLine();
-            ParseLevelType(type);
+
+            // parseamos el tipo de nivel
+            string[] line= type.Split(' ');
+            mLevelType_= (TypeLevel)Enum.Parse(typeof(TypeLevel), Text.ToUpper(line[0]));
+            arg= int.Parse(line[1]);
+
+            // leemos lo temporal
+            int i=0;
+            while(i<tmp.Length && !read.EndOfStream){
+                tmp[i]=read.ReadLine();
+                line= type.Split(' ');
+                i++;
+                cols=line.Length;
+            }
+            fils=i;
+
+            return tmp;
             
 
         }
         else{
             Debug.LogError("archivo no encontrado");
         }
+        return null;
         
     }
 
-    private void ParseLevelType(string type){
 
-        string[] line= type.Split(' ');
-        mLevelType_= (TypeLevel)Enum.Parse(typeof(TypeLevel), Text.ToUpper(line[0]));
-        arg= int.Parse(line[1]);
-    }
 }
--- Pharadrop/Assets/Scripts/GridParser/LevelManager.cs
+++ Pharadrop/Assets/Scripts/GridParser/LevelManager.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string mLevelRoute;
+    GridReader mGrid;
+    TypeLevel mType_;
+
+    public char [] mKeyChars;
+    public GameObject [] mBalls;
+    private string[] mLevel;
     void Start()
     {
+        mGrid= new GridReader();
+        mLevel= mGrid.readFile(mLevelRoute);
+        mType_= mGrid.getLevelType();
         
+
+        for(int i=0;i<mLevel.Length;i++){
+            string [] line= mLevel[i].Split(' ');
+            for(int j=0 ; j<line.Length; j++){
+                
+            } 
+        }
+
     }
 
     // Update is called once per frame
