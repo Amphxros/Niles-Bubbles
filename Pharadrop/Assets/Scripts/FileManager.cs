067177a2c8aa3b5f27c11630a5397c4f4f446610
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-07 22:58:27 +0100

    lectura de datos correcta

--- Pharadrop/Assets/Scripts/FileManager.cs
+++ Pharadrop/Assets/Scripts/FileManager.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-enum LevelType{ HP, TIME, SCORE}
+public enum LevelType{ HP, TIME, SCORE, MOVES}
+
+public struct LevelData{
+    public LevelType type;
+    public int args;
+}
 public class FileManager : MonoBehaviour
 {
+
     string [] namesArray;
     string mFilePath;
     public string filename;
 
+    LevelData mData;
+    
     void Start()
     {
         mFilePath=Application.dataPath +"/" + filename;
@@ -18,14 +26,42 @@ public class FileManager : MonoBehaviour
     }
 
     public void ReadFromFile(){
+       
         StreamReader read= new StreamReader(mFilePath);
         if(File.Exists(mFilePath)){
             string line= read.ReadLine();
             string [] split= line.Split(' ');
 
-            Debug.Log(split[0] + " ");
+            switch (split[0])
+            {
+                case "HP":
+                mData.type= LevelType.HP;
+                break;
 
-          
+                case "TIME":
+                mData.type= LevelType.TIME;    
+                break;
+                
+                case "SCORE":
+                mData.type= LevelType.SCORE;
+                break;
+                
+                case "MOVES":
+                mData.type=LevelType.MOVES;
+                break;
+               
+            }
+
+            mData.args= int.Parse(split[1]);
+
+            string [] data= new string[100];
+            int fils= 0;
+            while(fils<data.Length && !read.EndOfStream){
+                line= read.ReadLine();
+                data[fils]= line;
+                fils++;
+                Debug.Log(line);
+            }          
         }
         else{
             Debug.LogError("No existe fichero" + filename);
