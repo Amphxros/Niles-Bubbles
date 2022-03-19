412e0938c7b6f5330d8e6ff2f46a738145aa03aa
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-19 16:45:57 +0100

    Principio de la grid

--- Pharadrop/Assets/Scripts/Ball/BallInGrid.cs
+++ Pharadrop/Assets/Scripts/Ball/BallInGrid.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class BallInGrid : MonoBehaviour
 {
-    
+
+    public int BallID=0;
+
+
     private int row_;
     public int getRow(){
         return row_;
     }
     public void setRow(int row){row_=row;}
-    private int col_;
     
+    
+    private int col_;
     public int getCol(){
         return col_;
     }
