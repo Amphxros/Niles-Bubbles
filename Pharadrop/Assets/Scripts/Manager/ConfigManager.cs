22f8249af9e401bb263033b275820a1bc827137a
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-03-29 17:09:16 +0200

    configuraciones cuasi funcionales

--- Pharadrop/Assets/Scripts/Manager/ConfigManager.cs
+++ Pharadrop/Assets/Scripts/Manager/ConfigManager.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 public class ConfigManager : MonoBehaviour
 {
+
+    private Resolution mResolution_;
+    private int mBGMVolume_;
+    private int mSFXVolume_;
+    
+
+
     public Text resolutions_text;
     public Text bgm_text;
     public Text sfx_text;
     
     bool fullscreen=false;
 
-    Resolution[] mResolutions;
+    Resolution[] mResolutions; //list of all the resolutions possibles
 
     int mResIndex;
     int mVolIndex;
     int mSfxIndex;
+
     void Awake()
     {
         mResIndex=0;
@@ -24,8 +32,22 @@ public class ConfigManager : MonoBehaviour
         updateResText();
     }
 
-    void updateResText(){
+    /// <summary>
+    /// This function is called when the object becomes enabled and active.
+    /// </summary>
+    void OnEnable()
+    { 
+        updateResText();
+        mResolution_= Screen.currentResolution;
+    }
+
+    public void ResetConfig(){
         
+        print("Reset");
+        Screen.SetResolution(mResolution_.width, mResolution_.height,false);
+    }
+ 
+    void updateResText(){
         mResolutions = Screen.resolutions;
         Resolution mRes= Screen.currentResolution;
         resolutions_text.text=mRes.width + "x" + mRes.height;
@@ -38,39 +60,51 @@ public class ConfigManager : MonoBehaviour
     }
 
     public void LessRes(){
+        print("Less Resolution");
         if(mResIndex>0){
             mResIndex--;
-            print(mResIndex);
-            Screen.SetResolution(mResolutions[mResIndex].width, mResolutions[mResIndex].height, true);
+            print(mResolutions[mResIndex].width +"x"+ mResolutions[mResIndex].height);
+            Screen.SetResolution(mResolutions[mResIndex].width, mResolutions[mResIndex].height, false);
+            Resolution mRes= Screen.currentResolution;
+            resolutions_text.text=mRes.width + "x" + mRes.height;
         }
+       
 
         updateResText();
     }
     
     public void MoreRes(){
-        if(mResIndex < mResolutions.Length){
+        
+        print("More Resolution");
+        if(mResIndex < mResolutions.Length -1){
             mResIndex++;
-            Screen.SetResolution(mResolutions[mResIndex].width, mResolutions[mResIndex].height, true);
+            print(mResolutions[mResIndex].width +"x"+ mResolutions[mResIndex].height);
+            Screen.SetResolution(mResolutions[mResIndex].width, mResolutions[mResIndex].height, false);
         }
         updateResText();
     }
 
 
     public void LessBGMVolume(){
+        
+        print("Less Volume");
 
     }
     
     public void MoreBGMVolume(){
         
+        print("MoreVolume");
     }
 
 
     public void LessSFXVolume(){
-
+        
+        print("Less SFX Volume");
     }
     
     public void MoreSFXVolume(){
         
+        print("More SFX volume");
     }
 
 
--- Pharadrop/Assets/Scripts/Manager/GameManager.cs
+++ Pharadrop/Assets/Scripts/Manager/GameManager.cs
@@ -6,14 +6,20 @@ using UnityEngine.SceneManagement;
 enum State {GAMESCREEN, MENU, CHARACTER_SELECT, LEVEL_SELECT, DIALOG_SCENE, GAME}
 public class GameManager : MonoBehaviour
 {
+    private int bgm_volume, sfx_volume;
     private static GameManager mInstance_;
     public static GameManager getInstance() {  return mInstance_;}
+
+
+    public ConfigManager mConfigManager_;
+
 
     private State mState;
 
     
     void Awake()
     {
+        
         if(mInstance_==null){
             mInstance_=this;
              DontDestroyOnLoad(this.gameObject);
@@ -21,54 +27,23 @@ public class GameManager : MonoBehaviour
         else{
             Destroy(this);
         }
+        mConfigManager_= null;
         mState= State.GAMESCREEN;
-    }
-
-    void Start(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    public ConfigManager getConfig(){
+        return mConfigManager_;
+    }
+
+    public void setConfigManager(ConfigManager config){
+        mConfigManager_=config;
     }
 
     public void ChangeScene(string name){
         SceneManager.LoadScene(name);
-        OnSceneChanged();
+        
     }
 
-    void OnSceneChanged(){
-        switch (mState)
-        {
-            case State.GAMESCREEN:
-                mState= State.MENU;
-            break;
-
-            case State.MENU:
-                mState= State.CHARACTER_SELECT;
-            break;
-
-            case State.CHARACTER_SELECT:
-                mState= State.LEVEL_SELECT;
-            break;
-
-            
-            case State.LEVEL_SELECT:
-                mState= State.DIALOG_SCENE;
-            break;
-
-            case State.DIALOG_SCENE:
-                mState= State.GAME;
-            break;
-
-            case State.GAME:
-                mState= State.LEVEL_SELECT;
-            break;
-
-            default:
-                break;
-        }
-    }
 }
