c94cb299b2731598f555a68d44ef57713bb46f88
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-06-02 11:32:04 +0200

    Menu de seleccion de personajes terminado

---
+++ Pharadrop/Assets/Scripts/UI/CharacterSelector.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CharacterSelector : MonoBehaviour
+{
+   public GameObject [] characters;
+   int ind=0;
+    void Start()
+    {
+        for(int i=0; i<characters.Length; i++)
+        {
+            characters[i].SetActive(false);
+        }
+
+        characters[ind].SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextCharacter(){
+        characters[ind].SetActive(false);
+        ind= (ind+1)%characters.Length;
+        characters[ind].SetActive(true);
+    }
+    public void BeforeCharacter(){
+        characters[ind].SetActive(false);
+        ind= (ind-1)%characters.Length;
+        characters[ind].SetActive(true);
+    
+    }
+}
