055dbd523e545e3407ff0b3460e676a264c425de
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-06-03 19:17:11 +0200

    El sistema de grid está funcional

--- Pharadrop/Assets/Scripts/Grid.cs
+++ Pharadrop/Assets/Scripts/Grid.cs
@@ -4,20 +4,81 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    Vector3 initPosition= new Vector3(-5,3,0);
-    private GameObject[,] balls;
+    Vector3 initPosition;
+    public GameObject[] balls;
+    private GameObject[,] grid;
     public int ROWS=5;
     public int COLS=5;
-
-
     public Vector3 init(){return initPosition;}
+
+    public FileManager manager;
     void Start()
     {
-        balls= new GameObject[ROWS, COLS];
+        initPosition= new Vector3(0,0,0);
+       build();
+    }
+
+    void build(){
+
+        string [] level= manager.getData();
+        ROWS= 20;
+        COLS= level[0].Split(' ').Length;
+        grid= new GameObject[ROWS, COLS];
 
         for(int i=0; i<ROWS; i++){
-            for(int j=0; j<COLS; j++){
-                balls[i,j]=null;
+
+            for(int j=0; j<COLS;j++){
+                grid[i,j]=null;
+            }
+        }
+
+        for(int i=0; i<level.Length; i++){
+
+            string[] line= level[i].Split(' ');
+
+            for(int j=0; j<line.Length; j++){
+                
+                GameObject g= null;
+                float x= (float)(j + 0.5f*j*(i%2));
+
+                if(i%2==0){
+                    x=j +1.0f;
+                }
+                else{
+                    x= j +0.5f;
+                }
+
+
+                switch(line[j]){
+                    case "A":
+               
+                        g= Instantiate(balls[0], transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                
+                    case "B":
+                    
+                        g= Instantiate(balls[1], transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                    case "C":
+                        g= Instantiate(balls[2],  transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                
+                    case "D":
+                    
+                        g= Instantiate(balls[3],  transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                    case "E":
+                        g= Instantiate(balls[4],  transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                
+                    case "F":
+                        g= Instantiate(balls[5],  transform.position + new Vector3(x,i,0), Quaternion.identity, this.transform) as GameObject;
+                    break;
+                }
+
+                grid[i,j]=g;
+
+                g=null;
             }
         }
     }
@@ -28,63 +89,25 @@ public class Grid : MonoBehaviour
     }
 
     public void attachBall(GameObject g,int i, int j){
-        balls[i,j]=g;
-        balls[i,j].transform.position= new Vector3(initPosition.x + i + i%2*(0.5f), initPosition.y+j, 0);
+       grid[i,j]=g;
+       grid[i,j].transform.position= new Vector3(initPosition.x + i + i%2*(0.5f), initPosition.y+j, 0);
         BallMovement b= g.GetComponent<BallMovement>();
         b.Desactivate();
     }
 
     public void detachBall(int i, int j){
-       GameObject g= balls[i,j];
+       GameObject g= grid[i,j];
        Destroy(g);
-       balls[i,j]=null;
+       grid[i,j]=null;
 
 
     }
 
-    public List<GameObject> getNeightbours(int i, int j){
-        List<GameObject> obj= new List<GameObject>();
+    // public List<GameObject> getNeightbours(int i, int j){
+    //  List<GameObject> res= new List<GameObject>();
 
-        if(i>0){
-            obj.Add(balls[i-1,j]);
-            if(j>0){
-                obj.Add(balls[i-1,j-1]);
-            }
-            if(j<ROWS-1){
-                obj.Add(balls[i-1,j+1]);
-            }
-        }
-        if(j>0){
-            obj.Add(balls[i,j-1]);
-             if(i>0){
-                obj.Add(balls[i-1,j-1]);
-            }
-            if(i<COLS-1){
-                obj.Add(balls[i+1,j-1]);
-            }
+    //  return res;
+    // }
 
-        }
-        if(i<COLS){
-            obj.Add(balls[i+1,j]);
-            if(j>0){
-                obj.Add(balls[i+1,j-1]);
-            }
-            if(j<ROWS-1){
-                obj.Add(balls[i+1,j+1]);
-            }
-        }
-        if(j<ROWS){
-             obj.Add(balls[i,j+1]);
-              if(i>0){
-                obj.Add(balls[i-1,j+1]);
-            }
-            if(i<COLS-1){
-                obj.Add(balls[i+1,j+1]);
-            }
-
-        }
-
-        return obj;
-
-    }
+    
 }
